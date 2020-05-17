8b82dc17893531f38623f12dfd03f179cdddc450
Maksym Poskannyi <max2000pl@gmail.com>
2020-05-17 14:53:09 +0300

    added transfer to the modal options window and the logic of modal window closing

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const modal = $modal()
+// передаем базовые опции в объекте (options)
+
+const modal = $modal({
+     //параметризация модального окна
+     title: 'max200pl modal',
+     closable: true,
+     content:
+          `
+     <h4>Modal is working</h4>
+     <p>Lorem ipsum dolor sit</p>
+     `,
+     width: '400px'
+})
