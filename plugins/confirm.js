e1c38881776903d602dbea4b1ef01921cbb611e6
Maksym Poskannyi <max2000pl@gmail.com>
2020-05-23 12:33:13 +0300

    commenting on the code 2

--- plugins/confirm.js
+++ plugins/confirm.js
@@ -1,21 +1,19 @@
+//? МЕТОД КОТОРЫЙ ПРИНИМАЕТ ПАРАМЕТРЫ И ВОЗВРАЩАЕТ ПАРАМЕТРЫ 
+
 $.confirm = function (options) {
      /**
       * Обработка нажатия кнопок (асинхронная) поэтому применяем Promise
       */
      return new Promise((resolve, reject) => { // возвращаем новый promise 
           //вернуть новое обещание ((решить, отклонить) => {// возвращаем обещание
+
+          setTimeout(() => modal.open(), 100) // открываем модальное окно по истечению времени 
+
           const modal = $modal({ // создаем новый элемент модалки 
                title: options.title,
                width: '400px',
                closable: false,
                content: options.content,
-
-               // когда окно закрывается тогда мы уничтожаем модальное окно 
-               onClose() {
-                    modal.destroy()
-               },
-               // 
-
                footerButtons: [
                     {
                          text: 'Cancel', type: 'secondary', handler() {
@@ -29,8 +27,11 @@ $.confirm = function (options) {
                               resolve() // если удалить вызываем метод resolve()
                          }
                     }
-               ]
+               ],
+               // когда окно закрывается тогда мы уничтожаем модальное окно 
+               onClose() {
+                    modal.destroy()
+               },
           })
-          setTimeout(() => modal.open(), 100) // открываем модальное окно по истечению времени 
      })
 }
