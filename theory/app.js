b4ef04229fa1dcaaba356b9e3331aa8cbafd297c
Maksym Poskannyi <max2000pl@gmail.com>
2020-06-09 17:09:44 +0300

    learn  XMLHttpRequest

--- theory/app.js
+++ theory/app.js
@@ -1,33 +1,42 @@
 //?  Fetch, XMLHttpRequest (XHR), Ajax
+/**
+ ** 1) задача для отправки различных запросов на сервер 
+ ** 2) requestURL -->  URL для асинхронных запросов на сервер 
+ ** 
+*/
 
-const requestURL = 'https://jsonplaceholder.typicode.com/users'
+//* EXAMPLE 1 -- задача для отправки различных запросов на сервер 
 
+const requestURL = 'https://jsonplaceholder.typicode.com/users' // -> для тестирования и прототипирования 
 
 
 function sendRequest(method, url, body = null) {
      return new Promise((resolve, reject) => {
           const xhr = new XMLHttpRequest()
 
-          xhr.open(method, url)
+          xhr.open(method, url)  // --> .open -- открытие нового соединения (method --> что именно нужно сделать (GET, POST, PUT...))
 
-          xhr.responseType = 'json'
+          xhr.responseType = 'json' // -> привести к типу  'json'  (массив объектов)
 
+          xhr.setRequestHeader('Content-Type', 'application/json')
+
+          //* Если успешно попадаем на сервер 
           xhr.onload = () => {
                // console.log(JSON.parse(xhr.response));
                //console.log(xhr.response);
 
-               if (xhr.status >= 400) {
-                    resolve(xhr.response);
+               if (xhr.status >= 400) {   //* Если статус ответа сервера  выше 400 ошибка 
+                    reject(xhr.response)
                } else {
-                    resolve(xhr.response);
+                    resolve(xhr.response)
                }
           }
-
-          xhr.onerror = () => {
-               reject(xhr.response);
+          //* Если ошибка 
+          xhr.onerror = () => {  // 
+               reject(xhr.response)
           }
 
-          xhr.send(body)
+          xhr.send(JSON.stringify(body)) // --> .send -- отправляем 
      })
 }
 
